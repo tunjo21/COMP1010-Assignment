c097356a2db12bf167b72a71e8fbd4b22e1a4254
Roses098 <161680674+Roses098@users.noreply.github.com>
2024-10-27 23:22:42 +1100

    Update is shuffle working.java

--- is shuffle working.java
+++ is shuffle working.java
@@ -6,42 +6,42 @@ public class Playlist {
     public Album album;
     public int songIndex;
 
-    public Playlist(Album album){
+    public Playlist(Album album){ // Initialize the list
         this.album = album;
         this.songIndex=0;
     }
 
-    public void play() {
-        Song song = album.AlbumSongs.get(songIndex);
+    public void play() { // Method to play the song
+        Song song = album.AlbumSongs.get(songIndex); // gets the song at the index it is sitting on
         song.play();
     }
 
-    public void next(){
-        if(songIndex == album.AlbumSongs.size()-1 ){
+    public void next(){ // Method goes to the next song
+        if(songIndex == album.AlbumSongs.size()-1 ){// if the next song is the last song on the list goes to the first song
             songIndex = 0;
         }
         else{
-        songIndex = songIndex + 1;
+        songIndex = songIndex + 1; // Goes to the next song
         }
     }
 
-    public void previous(){
-        if(songIndex == 0 ){
+    public void previous(){ // Method goes to the previous song 
+        if(songIndex == 0 ){ // if the previous song is the first song on the list goes to the last song 
             songIndex = album.AlbumSongs.size() - 1;
         }
         else{
-        songIndex = songIndex - 1;
+        songIndex = songIndex - 1; // Goes to previous song 
         }
     }
 
-    public void shuffle(){
-        Integer[] intArray=new Integer[album.AlbumSongs.size()];
-        for(int i=0;i<album.AlbumSongs.size();i++){
+    public void shuffle(){ // method shuffles the songs 
+        Integer[] intArray=new Integer[album.AlbumSongs.size()]; // Creating an array to hold the song index
+        for(int i=0;i<album.AlbumSongs.size();i++){ // creating indexes for the array to hold 
             intArray[i]=i;
         }
-        List<Integer> intlist=Arrays.asList(intArray);
+        List<Integer> intlist=Arrays.asList(intArray); // making this a list to allow shuffling 
         Collections.shuffle(intlist);     
-        for(int i=0;i<album.AlbumSongs.size();i++){
+        for(int i=0;i<album.AlbumSongs.size();i++){ // rearanging song based on index
             album.AlbumSongs.set(i,album.AlbumSongs.get(intlist.get(i)));
         }
     }
